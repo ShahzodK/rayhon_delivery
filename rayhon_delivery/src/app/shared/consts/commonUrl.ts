c5a3e1e88830fa0414b8d9adae8f08d248d4c7be
Shahzod Kakhkhorov <57046438+ShahzodK@users.noreply.github.com>
2023-09-06 22:19:32 +0500

    Add // to url

--- rayhon_delivery/src/app/shared/consts/commonUrl.ts
+++ rayhon_delivery/src/app/shared/consts/commonUrl.ts
@@ -1,5 +1,5 @@
 export let CommonUrl = {
-    MAIN_URL: 'https:champslab.tech/api/v1/customers/',
+    MAIN_URL: 'https://champslab.tech/api/v1/customers/',
     LOGIN_URL: 'login',
     VERIFY_OTP_URL: 'verify-otp',
     USER_URL: 'me',
