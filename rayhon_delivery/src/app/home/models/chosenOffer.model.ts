7f3d307b262c955d7e0b768e4b36ca72bbdd433e
Shahzod Kakhkhorov <57046438+ShahzodK@users.noreply.github.com>
2024-01-06 16:59:52 +0500

    feat: delete popular and all meals chip in search page

---
+++ rayhon_delivery/src/app/home/models/chosenOffer.model.ts
@@ -0,0 +1,11 @@
+export interface IChosenOffer {
+    id: string,
+    name: string,
+    description: string,
+    image: string,
+    order_button_text: string,
+    order_button_color_hex: string,
+    order_button_action: string,
+    item: string,
+    category: string
+}
